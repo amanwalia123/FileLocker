cc4ff2400983e4c69ff4b8aba5ff3560025ae1a0
aman <amanpreetwalia278@gmail.com>
2018-02-13 22:13:21 -0500

    Some Changes

--- lock.sh
+++ lock.sh
@@ -4,8 +4,10 @@ read -s password
 for file in *
 #for file in *.{jpg,gif,png,jpeg}
 do
-echo $file
-   openssl enc -aes-256-cbc -salt -in $file -out "$file=enc" -k $password
-   rm -f $file    
+   if ! [[ $file == *.sh ]]; then
+	   echo $file
+	   openssl enc -aes-256-cbc -salt -in $file -out "$file=enc" -k $password
+	   rm -f $file  
+   fi  
 done;
    
